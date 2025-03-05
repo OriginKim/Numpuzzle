2da6500fec477ca50c74471a7f47c2cacd09aaeb
OriginKim <gohouse@gmail.com>
2025-03-06 00:52:03 +0900

    Update files

---
+++ GUI_Final_Project_GameClient/puzzle/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace GUI_Final_Project_GameClient
+{
+    partial class GUI_Final_Project_GameClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupPuzzleBox = new System.Windows.Forms.GroupBox();
+            this.pbx1 = new System.Windows.Forms.PictureBox();
+            this.pbx2 = new System.Windows.Forms.PictureBox();
+            this.pbx3 = new System.Windows.Forms.PictureBox();
+            this.pbx4 = new System.Windows.Forms.PictureBox();
+            this.pbx5 = new System.Windows.Forms.PictureBox();
+            this.pbx6 = new System.Windows.Forms.PictureBox();
+            this.pbx7 = new System.Windows.Forms.PictureBox();
+            this.pbx8 = new System.Windows.Forms.PictureBox();
+            this.pbx9 = new System.Windows.Forms.PictureBox();
+            this.labelMove = new System.Windows.Forms.Label();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.tmrTimeElapse = new System.Windows.Forms.Timer(this.components);
+            this.buttonGameReady = new System.Windows.Forms.Button();
+            this.groupBoxOriginal = new System.Windows.Forms.GroupBox();
+            this.labelGameExplain = new System.Windows.Forms.Label();
+            this.groupPuzzleBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupPuzzleBox
+            // 
+            this.groupPuzzleBox.Controls.Add(this.pbx1);
+            this.groupPuzzleBox.Controls.Add(this.pbx2);
+            this.groupPuzzleBox.Controls.Add(this.pbx3);
+            this.groupPuzzleBox.Controls.Add(this.pbx4);
+            this.groupPuzzleBox.Controls.Add(this.pbx5);
+            this.groupPuzzleBox.Controls.Add(this.pbx6);
+            this.groupPuzzleBox.Controls.Add(this.pbx7);
+            this.groupPuzzleBox.Controls.Add(this.pbx8);
+            this.groupPuzzleBox.Controls.Add(this.pbx9);
+            this.groupPuzzleBox.Location = new System.Drawing.Point(44, 11);
+            this.groupPuzzleBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupPuzzleBox.Name = "groupPuzzleBox";
+            this.groupPuzzleBox.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupPuzzleBox.Size = new System.Drawing.Size(498, 393);
+            this.groupPuzzleBox.TabIndex = 0;
+            this.groupPuzzleBox.TabStop = false;
+            this.groupPuzzleBox.Enter += new System.EventHandler(this.gbPuzzleBox_Enter);
+            // 
+            // pbx1
+            // 
+            this.pbx1.Location = new System.Drawing.Point(20, 21);
+            this.pbx1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pbx1.Name = "pbx1";
+            this.pbx1.Size = new System.Drawing.Size(152, 120);
+            this.pbx1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx1.TabIndex = 0;
+            this.pbx1.TabStop = false;
+            this.pbx1.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx2
+            // 
+            this.pbx2.Location = new System.Drawing.Point(174, 21);
+            this.pbx2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pbx2.Name = "pbx2";
+            this.pbx2.Size = new System.Drawing.Size(152, 120);
+            this.pbx2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx2.TabIndex = 0;
+            this.pbx2.TabStop = false;
+            this.pbx2.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx3
+            // 
+            this.pbx3.Location = new System.Drawing.Point(328, 21);
+            this.pbx3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pbx3.Name = "pbx3";
+            this.pbx3.Size = new System.Drawing.Size(152, 120);
+            this.pbx3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx3.TabIndex = 0;
+            this.pbx3.TabStop = false;
+            this.pbx3.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx4
+            // 
+            this.pbx4.Location = new System.Drawing.Point(20, 143);
+            this.pbx4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pbx4.Name = "pbx4";
+            this.pbx4.Size = new System.Drawing.Size(152, 120);
+            this.pbx4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx4.TabIndex = 0;
+            this.pbx4.TabStop = false;
+            this.pbx4.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx5
+            // 
+            this.pbx5.Location = new System.Drawing.Point(174, 143);
+            this.pbx5.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pbx5.Name = "pbx5";
+            this.pbx5.Size = new System.Drawing.Size(152, 120);
+            this.pbx5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx5.TabIndex = 0;
+            this.pbx5.TabStop = false;
+            this.pbx5.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx6
+            // 
+            this.pbx6.Location = new System.Drawing.Point(328, 143);
+            this.pbx6.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pbx6.Name = "pbx6";
+            this.pbx6.Size = new System.Drawing.Size(152, 120);
+            this.pbx6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx6.TabIndex = 0;
+            this.pbx6.TabStop = false;
+            this.pbx6.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx7
+            // 
+            this.pbx7.Location = new System.Drawing.Point(20, 265);
+            this.pbx7.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pbx7.Name = "pbx7";
+            this.pbx7.Size = new System.Drawing.Size(152, 120);
+            this.pbx7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx7.TabIndex = 0;
+            this.pbx7.TabStop = false;
+            this.pbx7.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx8
+            // 
+            this.pbx8.Location = new System.Drawing.Point(174, 265);
+            this.pbx8.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pbx8.Name = "pbx8";
+            this.pbx8.Size = new System.Drawing.Size(152, 120);
+            this.pbx8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx8.TabIndex = 0;
+            this.pbx8.TabStop = false;
+            this.pbx8.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx9
+            // 
+            this.pbx9.Location = new System.Drawing.Point(328, 265);
+            this.pbx9.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pbx9.Name = "pbx9";
+            this.pbx9.Size = new System.Drawing.Size(152, 120);
+            this.pbx9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx9.TabIndex = 0;
+            this.pbx9.TabStop = false;
+            this.pbx9.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // labelMove
+            // 
+            this.labelMove.AutoSize = true;
+            this.labelMove.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMove.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.labelMove.Location = new System.Drawing.Point(72, 419);
+            this.labelMove.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelMove.Name = "labelMove";
+            this.labelMove.Size = new System.Drawing.Size(75, 19);
+            this.labelMove.TabIndex = 2;
+            this.labelMove.Text = "이동 횟수 : ";
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("Impact", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTime.Location = new System.Drawing.Point(663, 324);
+            this.labelTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(264, 80);
+            this.labelTime.TabIndex = 3;
+            this.labelTime.Text = "00:00:00";
+            // 
+            // tmrTimeElapse
+            // 
+            this.tmrTimeElapse.Enabled = true;
+            this.tmrTimeElapse.Interval = 900;
+            this.tmrTimeElapse.Tick += new System.EventHandler(this.UpdateTimeElapsed);
+            // 
+            // buttonGameReady
+            // 
+            this.buttonGameReady.Font = new System.Drawing.Font("굴림", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonGameReady.Location = new System.Drawing.Point(412, 387);
+            this.buttonGameReady.Name = "buttonGameReady";
+            this.buttonGameReady.Size = new System.Drawing.Size(244, 58);
+            this.buttonGameReady.TabIndex = 10;
+            this.buttonGameReady.Text = "준비완료...";
+            this.buttonGameReady.UseVisualStyleBackColor = true;
+            this.buttonGameReady.Click += new System.EventHandler(this.buttonGameStart_Click);
+            // 
+            // groupBoxOriginal
+            // 
+            this.groupBoxOriginal.BackgroundImage = global::GUI_Final_Project_GameClient.Properties.Resources.original;
+            this.groupBoxOriginal.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.groupBoxOriginal.Location = new System.Drawing.Point(591, 23);
+            this.groupBoxOriginal.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBoxOriginal.Name = "groupBoxOriginal";
+            this.groupBoxOriginal.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBoxOriginal.Size = new System.Drawing.Size(415, 298);
+            this.groupBoxOriginal.TabIndex = 0;
+            this.groupBoxOriginal.TabStop = false;
+            this.groupBoxOriginal.Text = "Original";
+            this.groupBoxOriginal.Enter += new System.EventHandler(this.gbOriginal_Enter);
+            // 
+            // labelGameExplain
+            // 
+            this.labelGameExplain.AutoSize = true;
+            this.labelGameExplain.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.labelGameExplain.Location = new System.Drawing.Point(23, 54);
+            this.labelGameExplain.Name = "labelGameExplain";
+            this.labelGameExplain.Size = new System.Drawing.Size(1031, 270);
+            this.labelGameExplain.TabIndex = 13;
+            this.labelGameExplain.Text = "맞춰라 퍼즐!(3인용)\r\n\r\n<게임방법>\r\n9개의 퍼즐로 나누어진 그림을 먼저 맞추는 사람이 이기는 게임입니다.\r\n화면에는 퍼즐이 무작위로 배치되" +
+    "며 하나의 빈칸을 이용해 타일을 이동하십시오!\r\n잠시 후 화면이 바뀌면 바로 시작됩니다!\r\n\r\n게임방법을 읽으신 후 하단의 \"준비 완료!\" 버튼" +
+    "을 눌러 다른 플레이어를 대기하세요!\r\n\r\n\r\n";
+            // 
+            // GUI_Final_Project_GameClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1066, 457);
+            this.Controls.Add(this.labelGameExplain);
+            this.Controls.Add(this.buttonGameReady);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.labelMove);
+            this.Controls.Add(this.groupBoxOriginal);
+            this.Controls.Add(this.groupPuzzleBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "GUI_Final_Project_GameClient";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "맞춰라 퍼즐!";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AskPermissionBeforeQuite);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupPuzzleBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbx1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx9)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupPuzzleBox;
+        private System.Windows.Forms.GroupBox groupBoxOriginal;
+        private System.Windows.Forms.PictureBox pbx3;
+        private System.Windows.Forms.PictureBox pbx9;
+        private System.Windows.Forms.PictureBox pbx6;
+        private System.Windows.Forms.PictureBox pbx2;
+        private System.Windows.Forms.PictureBox pbx8;
+        private System.Windows.Forms.PictureBox pbx5;
+        private System.Windows.Forms.PictureBox pbx1;
+        private System.Windows.Forms.PictureBox pbx7;
+        private System.Windows.Forms.PictureBox pbx4;
+        private System.Windows.Forms.Label labelMove;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Timer tmrTimeElapse;
+        private System.Windows.Forms.Button buttonGameReady;
+        private System.Windows.Forms.Label labelGameExplain;
+    }
+}
+
---
+++ GUI_Final_Project_GameClient/puzzle/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GUI_Final_Project_GameClient
+{
+    public partial class GUI_Final_Project_GameClient : Form
+    {
+        int inNullSliceIndex, inmoves = 0;
+        List<Bitmap> lstOriginalPictureList = new List<Bitmap>();
+        System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
+
+        TcpClient client;
+        NetworkStream stream;
+        Thread listenThread;
+
+        public GUI_Final_Project_GameClient()
+        {
+            InitializeComponent();
+            lstOriginalPictureList.AddRange(new Bitmap[] { Properties.Resources._1, Properties.Resources._2, Properties.Resources._3, Properties.Resources._4, Properties.Resources._5, Properties.Resources._6, Properties.Resources._7, Properties.Resources._8, Properties.Resources._9, Properties.Resources._null });
+            labelMove.Text += inmoves;
+            labelTime.Text = "00:00:00";
+            SetInitialVisibility();
+
+            ConnectToServer();
+        }
+
+        private void SetInitialVisibility()
+        {
+            groupPuzzleBox.Visible = false;
+            labelMove.Visible = false;
+            labelTime.Visible = false;
+            labelGameExplain.Visible = true;
+           
+            groupBoxOriginal.Visible = false;
+            buttonGameReady.Visible = true; // 게임 시작 버튼을 표시
+        }
+
+        private void ConnectToServer()
+        {
+            try
+            {
+                client = new TcpClient("192.168.0.208", 5000);
+                stream = client.GetStream();
+                listenThread = new Thread(ListenForMessages);
+                listenThread.Start();
+            }
+            catch
+            {
+                MessageBox.Show("서버에 연결하지 못했습니다.");
+            }
+        }
+
+        private void ListenForMessages()
+        {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            try
+            {
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine($"Message received: {message}"); // 디버깅 메시지
+                    if (message == "start")
+                    {
+                        this.Invoke((MethodInvoker)delegate {
+                            StartGame();
+                        });
+                    }
+                    else if (message == "win")
+                    {
+                        this.Invoke((MethodInvoker)delegate {
+                            GameOver("축하합니다! 승리!\n계속하시겠습니까?");
+                            ResetGame();
+                        });
+                    }
+                    else if (message == "lose")
+                    {
+                        this.Invoke((MethodInvoker)delegate {
+                            GameOver("아쉽습니다! 패배!\n계속하시겠습니까?");
+                        });
+                    }
+                    else if (message == "regame")
+                    {
+                        this.Invoke((MethodInvoker)delegate {
+                            AskForReGame();
+                        });
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private void StartGame()
+        {
+            buttonGameReady.Visible = false;
+            groupPuzzleBox.Visible = true;
+            labelMove.Visible = true;
+            labelTime.Visible = true;
+            labelGameExplain.Visible = false;
+            groupBoxOriginal.Visible = true;
+            
+            timer.Reset();
+            timer.Start();
+        }
+
+
+        private void GameOver(string message)
+        {
+            timer.Stop();
+            var result = MessageBox.Show(message, "Game Over", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                SendMessage("regame");
+                ResetGame();
+            }
+            else
+            {
+                SendMessage("regame:no");
+                Application.Exit();
+            }
+        }
+
+        private void AskForReGame()
+        {
+            var result = MessageBox.Show("다시 게임에 참여 하시겠습니까?", "Game Over", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                SendMessage("regame:yes");
+                ResetGame();
+            }
+            else
+            {
+                SendMessage("regame:no");
+                Application.Exit();
+            }
+        }
+
+        private void ResetGame()
+        {
+            SetInitialVisibility();
+            Shuffle();
+            inmoves = 0;
+            labelMove.Text = "이동 횟수 : 0";
+            labelTime.Text = "00:00:00";
+            timer.Reset();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Shuffle();
+        }
+
+        void Shuffle()
+        {
+            do
+            {
+                int j;
+                List<int> Index = new List<int>(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 9 });
+                Random r = new Random();
+                for (int i = 0; i < 9; i++)
+                {
+                    Index.Remove(j = Index[r.Next(0, Index.Count)]);
+                    ((PictureBox)groupPuzzleBox.Controls[i]).Image = lstOriginalPictureList[j];
+                    if (j == 9) { inNullSliceIndex = i; }
+                }
+            }
+            while (CheckWin());
+        }
+
+        private void btnShuffle_Click(object sender, EventArgs e)
+        {
+            DialogResult YesOrNo = new DialogResult();
+            if (labelTime.Text != "00:00:00")
+            {
+                YesOrNo = MessageBox.Show("정말 다시 시작하시겠습니까?", "맞춰라 퍼즐", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            }
+            if (YesOrNo == DialogResult.Yes || labelTime.Text == "00:00:00")
+            {
+                Shuffle();
+                timer.Reset();
+                labelTime.Text = "00:00:00";
+                inmoves = 0;
+                labelMove.Text = "이동 횟수 : 0";
+            }
+        }
+
+        private void AskPermissionBeforeQuite(object sender, FormClosingEventArgs e)
+        {
+            DialogResult YesOrNO = MessageBox.Show("정말 종료하시겠습니까?", "맞춰라 퍼즐", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            
+            
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            AskPermissionBeforeQuite(sender, e as FormClosingEventArgs);
+        }
+
+        private void SwitchPictureBox(object sender, EventArgs e)
+        {
+            if (labelTime.Text == "00:00:00")
+                timer.Start();
+            int inPictureBoxIndex = groupPuzzleBox.Controls.IndexOf(sender as Control);
+            if (inNullSliceIndex != inPictureBoxIndex)
+            {
+                List<int> FourPictures = new List<int>(new int[] { ((inPictureBoxIndex % 3 == 0) ? -1 : inPictureBoxIndex - 1), inPictureBoxIndex - 3, (inPictureBoxIndex % 3 == 2) ? -1 : inPictureBoxIndex + 1, inPictureBoxIndex + 3 });
+                if (FourPictures.Contains(inNullSliceIndex))
+                {
+                    ((PictureBox)groupPuzzleBox.Controls[inNullSliceIndex]).Image = ((PictureBox)groupPuzzleBox.Controls[inPictureBoxIndex]).Image;
+                    ((PictureBox)groupPuzzleBox.Controls[inPictureBoxIndex]).Image = lstOriginalPictureList[9];
+                    inNullSliceIndex = inPictureBoxIndex;
+                    labelMove.Text = "이동 횟수 : " + (++inmoves);
+                    if (CheckWin())
+                    {
+                        SendMessage("GameOver"); // 게임 종료 메시지 전송
+                        Console.WriteLine("GameOver message sent"); // 디버깅 메시지
+                        timer.Stop();
+                        (groupPuzzleBox.Controls[8] as PictureBox).Image = lstOriginalPictureList[8];
+
+                        
+                        inmoves = 0;
+                        labelMove.Text = "Moves Made : 0";
+                        labelTime.Text = "00:00:00";
+                        timer.Reset();
+                    }
+                }
+            }
+        }
+
+        bool CheckWin()
+        {
+            int i;
+            for (i = 0; i < 8; i++)
+            {
+                if ((groupPuzzleBox.Controls[i] as PictureBox).Image != lstOriginalPictureList[i]) break;
+            }
+            if (i == 8)
+            {
+                
+                return true;
+            }
+            else
+            {
+                
+                return false;
+            }
+        }
+
+        private void UpdateTimeElapsed(object sender, EventArgs e)
+        {
+            if (timer.Elapsed.ToString() != "00:00:00")
+            {
+                labelTime.Text = timer.Elapsed.ToString().Remove(8);
+            }
+            
+        }
+
+        private void SendMessage(string message)
+        {
+            if (stream != null)
+            {
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine($"Message sent: {message}"); // 디버깅 메시지
+            }
+        }
+        private void gbPuzzleBox_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gbOriginal_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonGameStart_Click(object sender, EventArgs e)
+        {
+            SendMessage("start");
+        }
+
+        
+    }
+}
